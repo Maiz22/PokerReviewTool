db6d26ada99814477542017ba615c9111384b5d8
Matthias Schmidt <matthias.schmidt.92@gmx.de>
2024-12-16 20:38:00 +0100

    First working gui version. Txt file can be loaded and parsed via tkinter filedialog. Rounds can be selected. You can jump from action to action.

--- controller.py
+++ controller.py
@@ -12,47 +12,137 @@ class Controller:
     def __init__(self, view: View, model: Model) -> None:
         self.input_path = None
         self.all_rounds = []
+        self.active_round = None
+        self.round_number = 0
         self.view = view
         self.model = model
         self.init_view_buttons()
 
     def init_view_buttons(self) -> None:
+        """
+        Create controller functions to views bind events.
+        """
         self.view.path_button_on_click(self.get_input_path)
-        self.view.read_button_on_click(self.process_input_file)
+        self.view.round_dropdown_on_change(self.select_round)
+        self.view.prev_action_button_on_click(self.prev_action)
+        self.view.next_action_button_on_click(self.next_action)
 
     def get_input_path(self, event) -> None:
+        """
+        Open a txt file through the views function and pass it
+        to self.input_path.
+        By returning 'break' the button event will be reset.
+        """
         self.input_path = self.view.open_txt_file()
+        self.process_input_file(event=None)
         return "break"
 
     def parse_input_file(self) -> None:
+        """
+        Opens the .txt file, and creates a list of lines of the
+        text file. The txt_list passed to the returned
+        parse_list_to_rounds_of_actions functions.
+        """
         txt_list = parse_txt_to_list(path=self.input_path)
         return parse_list_to_rounds_of_actions(txt_list=txt_list)
 
     def process_input_file(self, event) -> None:
-        (
-            self.all_rounds,
-            total_actions,
-            total_known_actions,
-            total_unknown_actions,
-            unknown_lines,
-        ) = self.parse_input_file()
-        self.print_to_view(
-            self.create_summary(
-                total_actions, total_known_actions, total_unknown_actions
+        """
+        Processes the input file by parsing it, printing a
+        summary to the views textfield.
+        Returns 'break' to reset the views button.
+        """
+        if self.input_path:
+            (
+                self.all_rounds,
+                total_actions,
+                total_known_actions,
+                total_unknown_actions,
+                unknown_lines,
+            ) = self.parse_input_file()
+            self.print_to_view(
+                self.create_summary(
+                    total_actions, total_known_actions, total_unknown_actions
+                )
             )
-        )
+            self.view.round_number_dropdown.configure(
+                values=[str(num) for num in range(1, len(self.all_rounds) + 1)]
+            )
+            self.print_to_view(
+                content="Opened file:\n{}".format(self.input_path.split(r"/")[-1])
+            )
+            self.view.round_number_var.set("1")
+            self.select_round()
+        else:
+            self.view.round_number_dropdown.configure(values=[])
+            self.view.round_number_var.set("")
         return "break"
 
     def create_summary(
         self, total_actions: str, total_known_actions: str, total_unknown_actions: str
     ) -> str:
-        return "--------------------\nTotal Actions: {}\nKnown_actions: {}\nUnknown Actions: {}\n--------------------\n".format(
+        """
+        Creates a summary as a string with parsing statistics.
+        """
+        return "--------------------\nTotal Actions: {}\nKnown_actions: {}\nUnknown Actions: {}\n--------------------".format(
             total_actions, total_known_actions, total_unknown_actions
         )
 
-    def print_to_view(self, content) -> None:
-        self.view.text_field.insert("end", content)
+    def print_to_view(self, content: str) -> None:
+        """
+        Takes a string as content, inserts it to the views
+        textfield and scrolls down to the buttom of it.
+        """
+        self.view.text_field.insert("end", "{}\n".format(content))
         self.view.text_field.see("end")
 
+    def select_round(self, event=None) -> None:
+        try:
+            self.round_number = int(self.view.round_number_var.get())
+            self.active_round = self.all_rounds[self.round_number]
+            self.print_to_view("Selected Round {}".format(self.round_number))
+            self.action_index = 0
+            self.set_action_button_activation_state()
+        except IndexError:
+            print("Round {} not in all rounds.")
+
+    def set_action_button_activation_state(self) -> None:
+        if self.action_index == (len(self.active_round) - 1):
+            self.view.next_button.configure(state="disabled")
+            self.view.prev_button.configure(state="normal")
+            return
+        if self.action_index == 0:
+            self.view.prev_button.configure(state="disabled")
+            self.view.next_button.configure(state="normal")
+            return
+        self.view.prev_button.configure(state="normal")
+        self.view.next_button.configure(state="normal")
+        return
+
+    def next_action(self, event) -> None:
+        if self.action_index < (len(self.active_round) - 1):
+            self.action_index += 1
+            self.set_action_button_activation_state()
+            self.print_to_view(content=self.active_round[self.action_index])
+
+    def prev_action(self, event) -> None:
+        if self.action_index > 0:
+            self.action_index -= 1
+            self.set_action_button_activation_state()
+            self.print_to_view(content=self.active_round[self.action_index])
+
+    # def next_round(self) -> None:
+    #    if self.round_number < len(self.all_rounds - 1):
+    #        self.round_number += 1
+    #    self.activate_round()
+    #
+    # def prev_round(self) -> None:
+    #    if self.round_number > 0:
+    #        self.round_number -= 1
+    #    self.activate_round()
+
     def run(self) -> None:
+        """
+        Calls the views mainloop to start the gui.
+        """
         self.view.mainloop()
--- view.py
+++ view.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 from PIL import ImageTk, Image
 
@@ -8,14 +9,25 @@ class View(tk.Tk):
         super().__init__()
         self.title("PokerReview")
         self.resizable(False, False)
-        self.geometry("400x400")
+        # self.geometry("400x400")
 
         self.setup_frame = tk.Frame(self)
         self.setup_frame.pack(fill="both", expand=True, pady=10)
         self.path_button = tk.Button(self.setup_frame, text="Open handhistory")
         self.path_button.pack(side="left", padx=10)
-        self.read_txt_button = tk.Button(self.setup_frame, text="Read")
-        self.read_txt_button.pack(side="left", padx=10)
+        self.round_label = tk.Label(self.setup_frame, text="Select Round:")
+        self.round_label.pack(side="left", padx=10)
+        self.round_number_var = tk.StringVar()
+        self.round_number_dropdown = ttk.Combobox(
+            self.setup_frame, state="readonly", textvariable=self.round_number_var
+        )
+        self.round_number_dropdown.pack(side="left", padx=10)
+        self.action_label = tk.Label(self.setup_frame, text="Action:")
+        self.action_label.pack(side="left", padx=10)
+        self.prev_button = tk.Button(self.setup_frame, state="disabled", text="Prev")
+        self.prev_button.pack(side="left", padx=5)
+        self.next_button = tk.Button(self.setup_frame, state="disabled", text="Next")
+        self.next_button.pack(side="left", padx=5)
 
         self.text_frame = tk.Frame(self)
         self.text_frame.pack(fill="both", expand=True)
@@ -25,8 +37,14 @@ class View(tk.Tk):
     def path_button_on_click(self, callback) -> None:
         self.path_button.bind("<Button-1>", callback)
 
-    def read_button_on_click(self, callback) -> None:
-        self.read_txt_button.bind("<Button->", callback)
+    def round_dropdown_on_change(self, callback) -> None:
+        self.round_number_dropdown.bind("<<ComboboxSelected>>", callback)
+
+    def next_action_button_on_click(self, callback) -> None:
+        self.next_button.bind("<Button-1>", callback)
+
+    def prev_action_button_on_click(self, callback) -> None:
+        self.prev_button.bind("<Button-1>", callback)
 
     def open_txt_file(self) -> str:
         file_path = askopenfilename(filetypes=[("Text file", ".txt")])
